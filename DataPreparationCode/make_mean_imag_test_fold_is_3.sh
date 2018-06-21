4ed30469ba96348e84760c0c6f20b2f5b7f92303
binanhiasm <quangdat1511@gmail.com>
2018-06-21 21:44:49 +0700

    Update make_mean_imag_test_fold_is_3.sh

--- DataPreparationCode/make_mean_imag_test_fold_is_3.sh
+++ DataPreparationCode/make_mean_imag_test_fold_is_3.sh
@@ -1,6 +1,6 @@
-TOOLS=/home/dom/caffe/build/tools
-DATA=/home/dom/Downloads/AdienceFaces/lmdb/Test_fold_is_3/gender_train_lmdb
-OUT=/home/dom/Downloads/AdienceFaces/mean_image/Test_folder_is_3
+TOOLS=/home/quangdat1511/caffe/build/tools
+DATA=/home/quangdat1511/Downloads/AdienceFaces/lmdb/Test_fold_is_0/gender_train_lmdb
+OUT=/home/quangdat1511/Downloads/AdienceFaces/mean_image/Test_folder_is_3
 
 $TOOLS/compute_image_mean $DATA $OUT/mean.binaryproto
 
